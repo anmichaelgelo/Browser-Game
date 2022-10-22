0f08b87516ee6770d457be33ae3eae0a85f6faaf
anmichaelgelo <anmichaelgelo@gmail.com>
2022-10-21 19:50:38 -0700

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Browser-Game
+# quiztions
+A trivia browser game for all
---
+++ javascript/script.js
@@ -0,0 +1,21 @@
+feather.replace();
+
+$('button').on('click', playButtonSound);
+$('a').on('click', playButtonSound);
+$('input').on('click', playButtonSound);
+
+function playButtonSound(){
+    $('#btn-click')[0].play();
+}
+
+$('#btn-join').on('click', function(){
+    $('.modal').css('display', 'flex');
+});
+
+$('#btn-close').on('click', function(){
+    $('.modal').hide(150);
+});
+
+$('.card').on('click', function(){
+    window.location.href = 'subject-preview.html';
+});
